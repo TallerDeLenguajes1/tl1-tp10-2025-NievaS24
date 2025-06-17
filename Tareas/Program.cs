f3fd1fbd32f08db0694e78541f6436fba8351658
NievaS24 <santinieva24@gmail.com>
2025-06-17 13:28:21 -0300

    Mostrar tareas (Pendientes luego Completadas).

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,5 +1,24 @@
-﻿HttpClient client = new HttpClient();
+﻿using System.Text.Json;
+
+HttpClient client = new HttpClient();
 HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
 response.EnsureSuccessStatusCode();
 string responseBody = await response.Content.ReadAsStringAsync();
-Console.WriteLine(responseBody); // Para ver si funciona la solicitud GET
+List<Tarea> listTareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+//Muestro primero pendientes, luego completadas.
+foreach (Tarea tarea in listTareas)
+{
+    if (!tarea.completed) 
+    {
+        Console.WriteLine($"\nTitulo: {tarea.title}");
+        Console.WriteLine($"Estado: Pendiente");
+    }
+}
+foreach (Tarea tarea in listTareas)
+{
+    if (tarea.completed)
+    {
+        Console.WriteLine($"\nTitulo: {tarea.title}");
+        Console.WriteLine($"Estado: Completada");
+    } 
+}
