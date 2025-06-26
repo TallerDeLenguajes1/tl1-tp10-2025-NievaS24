280d33a170f0bcdec55effb6ba0299279e5b985f
NievaS24 <santinieva24@gmail.com>
2025-06-26 12:04:46 -0300

    Ejercicio 1 terminado.

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System.Text.Json;
 
+//Creo una instancia Http
 HttpClient client = new HttpClient();
+//Enviar solicitud GET
 HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
+//Verifico que la solicitud fue exitosa
 response.EnsureSuccessStatusCode();
+//Leo y deserealizo la respuesta
 string responseBody = await response.Content.ReadAsStringAsync();
 List<Tarea> listTareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
 //Muestro primero pendientes, luego completadas.
 foreach (Tarea tarea in listTareas)
 {
-    if (!tarea.completed) 
+    if (!tarea.completed)
     {
         Console.WriteLine($"\nTitulo: {tarea.title}");
         Console.WriteLine($"Estado: Pendiente");
@@ -20,5 +24,13 @@ foreach (Tarea tarea in listTareas)
     {
         Console.WriteLine($"\nTitulo: {tarea.title}");
         Console.WriteLine($"Estado: Completada");
-    } 
+    }
+}
+//Serealizo y guardo en tareas.json
+string ruta = Directory.GetCurrentDirectory(); //Consigo la direccion de donde estoy trabajando, me devuelve dentro de bin
+ruta = Directory.GetParent(ruta).Parent.Parent.FullName; //Salgo de net8.0 -> Debug -> bin
+using (StreamWriter sw = new StreamWriter(ruta + @"\tarea.json"))
+{
+    string jsonString = JsonSerializer.Serialize(listTareas);
+    sw.WriteLine(jsonString);
 }
