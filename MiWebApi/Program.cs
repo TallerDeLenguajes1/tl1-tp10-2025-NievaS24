ce23ecab8e67ef4ca84dee4f49b4f8256b4feff7
NievaS24 <santinieva24@gmail.com>
2025-06-27 12:46:37 -0300

    Peticion GET y deserealizacion hecha.

--- MiWebApi/Program.cs
+++ MiWebApi/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+HttpClient client = new HttpClient();
+string url = "https://api.marketstack.com/v2/eod?access_key=b3488c9a4291dc425d9efe9b197e7847&symbols=AAPL,AMZN,MELI,KO";
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync();
+MarketStack marketStacks = JsonSerializer.Deserialize<MarketStack>(responseBody);
+List<Data> marketDatas = marketStacks.data;
