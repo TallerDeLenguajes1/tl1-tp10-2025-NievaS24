dcf04ceb2c765df953137085575c8295dcf24cc2
NievaS24 <santinieva24@gmail.com>
2025-06-27 11:38:10 -0300

    arreglo visual de codigo.

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -6,12 +6,12 @@ HttpResponseMessage response = await client.GetAsync(url);
 response.EnsureSuccessStatusCode();
 string responseBody = await response.Content.ReadAsStringAsync();
 List<Usuario> usuarios = JsonSerializer.Deserialize<List<Usuario>>(responseBody);
-System.Console.WriteLine("\n\n **** Primeros 5 usuarios ****\n\n");
+Console.WriteLine("\n\n **** Primeros 5 usuarios ****\n\n");
 for (int i = 0; i < 5; i++)
 {
-    System.Console.WriteLine($"Usuario {i + 1}:");
-    System.Console.WriteLine($"Nombre: {usuarios[i].name}");
-    System.Console.WriteLine($"Correo electronico: {usuarios[i].email}");
-    System.Console.WriteLine($"Direccion:");
-    System.Console.WriteLine($"\t {usuarios[i].address.suite}, {usuarios[i].address.street}, {usuarios[i].address.city}\n\n");
+    Console.WriteLine($"Usuario {i + 1}:");
+    Console.WriteLine($"Nombre: {usuarios[i].name}");
+    Console.WriteLine($"Correo electronico: {usuarios[i].email}");
+    Console.WriteLine($"Direccion:");
+    Console.WriteLine($"\t {usuarios[i].address.suite}, {usuarios[i].address.street}, {usuarios[i].address.city}\n\n");
 }
