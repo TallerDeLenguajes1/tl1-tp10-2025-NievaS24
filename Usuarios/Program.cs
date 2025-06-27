9ea2daed2f60b44523791d191e45b9e6a05cbb7e
NievaS24 <santinieva24@gmail.com>
2025-06-27 11:34:42 -0300

    Ejercicio 2 terminado.

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+HttpClient client = new HttpClient();
+string url = "https://jsonplaceholder.typicode.com/users/";
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Usuario> usuarios = JsonSerializer.Deserialize<List<Usuario>>(responseBody);
+System.Console.WriteLine("\n\n **** Primeros 5 usuarios ****\n\n");
+for (int i = 0; i < 5; i++)
+{
+    System.Console.WriteLine($"Usuario {i + 1}:");
+    System.Console.WriteLine($"Nombre: {usuarios[i].name}");
+    System.Console.WriteLine($"Correo electronico: {usuarios[i].email}");
+    System.Console.WriteLine($"Direccion:");
+    System.Console.WriteLine($"\t {usuarios[i].address.suite}, {usuarios[i].address.street}, {usuarios[i].address.city}\n\n");
+}
